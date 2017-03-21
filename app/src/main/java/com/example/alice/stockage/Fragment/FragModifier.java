dce6a090c1ac4e07b4801fc3f51631039460a2ac
AliceP-R <alice.pottier@free.fr>
2017-03-21 14:02:03 +0100

    Ajout Fragment
    Attention, erreur quand utilisation des boutons dans la frameview

---
+++ app/src/main/java/com/example/alice/stockage/Fragment/FragModifier.java
@@ -0,0 +1,22 @@
+package com.example.alice.stockage.Fragment;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alice.stockage.R;
+
+public class FragModifier extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_modifier, container, false); // false => non affiché
+    }
+
+
+
+}
--- app/src/main/java/com/example/alice/stockage/Vue/Rechercher.java
+++ app/src/main/java/com/example/alice/stockage/Vue/Rechercher.java
@@ -1,27 +1,38 @@
 package com.example.alice.stockage.Vue;
 
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.alice.stockage.Donnees.Personne;
+import com.example.alice.stockage.Fragment.*;
 import com.example.alice.stockage.Personnalisation.ContactAdapter;
 import com.example.alice.stockage.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Rechercher extends AppCompatActivity {
 
     private ArrayList lPersonne;
     private ArrayList lRes;
     private ContactAdapter adapterContact;
+
+    private FragmentTransaction transaction;
+    private FragModifier fragModif;
+
+    Personne base;
 
     public class ListClickHandler implements AdapterView.OnItemClickListener {
 
@@ -30,9 +41,32 @@ public class Rechercher extends AppCompatActivity {
 
             TextView tvID = (TextView) view.findViewById(R.id.tvId);
             String text = tvID.getText().toString();
-            Intent intent = new Intent(parent.getContext(), Modification.class);
-            intent.putExtra("select-id", text);
-            startActivity(intent);
+
+            // si on est en portrait, on utilise la méthode "classique" d'appeler une autre activité
+            if (findViewById(R.id.idContenaireFrag) == null) {
+                Intent intent = new Intent(parent.getContext(), Modification.class);
+                intent.putExtra("select-id", text);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "frag", Toast.LENGTH_SHORT).show();
+
+
+
+                List<Personne> res = Personne.find(Personne.class, "id = " + text);
+                base = res.get(0);
+
+                EditText etNomF = (EditText) findViewById(R.id.txtNomMF);
+                EditText etPrenomF = (EditText) findViewById(R.id.txtPrenomMF);
+                EditText etTelF = (EditText) findViewById(R.id.txtTelMF);
+
+                etNomF.setText(base.getNom());
+                etPrenomF.setText(base.getPrenom());
+                etTelF.setText(base.getNumero());
+
+                transaction = getFragmentManager().beginTransaction();
+                transaction.show(fragModif);
+                transaction.commit();
+            }
         }
     }
 
@@ -56,6 +90,97 @@ public class Rechercher extends AppCompatActivity {
         ListView lv = (ListView) findViewById(R.id.lview);
         lv.setAdapter(adapterContact);
         lv.setOnItemClickListener(new ListClickHandler());
+
+        fragModif = new FragModifier();
+
+        // si le container est créé, on ajoute le fragment
+        if (findViewById(R.id.idContenaireFrag) != null) {
+            transaction = getFragmentManager().beginTransaction();
+            transaction.add(R.id.idContenaireFrag,fragModif);
+            transaction.addToBackStack(null);
+            transaction.hide(fragModif);
+            transaction.commit();
+
+            Button modif=(Button)findViewById(R.id.bModifF) ;
+            modif.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    EditText etNom = (EditText) findViewById(R.id.txtNomMF);
+                    EditText etPrenom = (EditText) findViewById(R.id.txtPrenomMF);
+                    EditText etTel = (EditText) findViewById(R.id.txtTelMF);
+
+                    String nomModif = etNom.getText().toString();
+                    String prenomModif = etPrenom.getText().toString();
+                    String telModif = etTel.getText().toString();
+
+                    // si click modif mais informations inchangée
+                    if(nomModif.equals(base.getNom()) && prenomModif.equals(base.getPrenom()) && telModif.equals(base.getNumero()))
+                    {
+                        Toast.makeText(getApplicationContext(), "Vous n'avez fait aucune modification !", Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        base.setNom(nomModif);
+                        base.setPrenom(prenomModif);
+                        base.setNumero(telModif);
+                        base.save();
+
+                        Toast.makeText(getApplicationContext(), "Modification faite !", Toast.LENGTH_LONG).show();
+
+                        transaction = getFragmentManager().beginTransaction();
+                        transaction.hide(fragModif);
+                        transaction.commit();
+
+                        adapterContact.notifyDataSetChanged();
+                    }
+                }
+            });
+
+            Button suppr=(Button)findViewById(R.id.bSupprF) ;
+            suppr.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                    builder.setTitle("Item");
+
+                    builder.setMessage("Voulez vous vraiment supprimer ce contact ?");
+                    builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            base.delete();
+                            Toast.makeText(getApplicationContext(), "Suppression faite !", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    });
+                    builder.setNegativeButton("Non", null);
+                    builder.show();
+                }
+            });
+
+            Button annul=(Button)findViewById(R.id.bAnnuleF) ;
+            annul.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    EditText etNomF = (EditText) findViewById(R.id.txtNomMF);
+                    EditText etPrenomF = (EditText) findViewById(R.id.txtPrenomMF);
+                    EditText etTelF = (EditText) findViewById(R.id.txtTelMF);
+
+                    etNomF.setText("");
+                    etPrenomF.setText("");
+                    etTelF.setText("");
+
+                    transaction = getFragmentManager().beginTransaction();
+                    transaction.hide(fragModif);
+                    transaction.commit();
+                }
+            });
+        }
     }
 
     private void recherche()
